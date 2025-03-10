59ffe86eab6f559931e25d25a78e78ca080c7a4e
wmahdi <mahdi.wouhaibi@ynov.com>
2025-03-10 16:32:49 +0100

    ajout de l'agent IA Mistral

---
+++ python/config.py
@@ -0,0 +1,4 @@
+MISTRAL_API_KEY = "6JHjskmRsviyDp18RjX5f4NQt7Oe1DrW"
+MISTRAL_AGENT_ID = "ag:037df95d:20250310:untitled-agent:aa72e83a"
+MISTRAL_API_URL = "https://api.mistral.ai/v1/chat/completions"  
+
